dbb8a02d4e770336e70a323ddc9b6eac924c1a8d
BlueDruddigon <haibuihoang1999@gmail.com>
2025-04-20 18:31:57 +0700

    fix(lsp): disable keybindings for signature help

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,35 +8,74 @@ return {
       -- python typeshed
       "python/typeshed",
     },
-    opts = {
-      codelens = { enabled = true },
-      server = {
-        basedpyright = {
-          settings = {
-            basedpyright = {
-              openFilesOnly = true,
-              analysis = {
-                logLevel = "Trace",
-                stubPath = vim.fn.stdpath("data") .. "/lazy/python-type-stubs",
-                extraPaths = {},
-                typeshedPaths = { vim.fn.stdpath("data") .. "/lazy/typeshed" },
-                autoSearchPaths = true,
-                diagnosticMode = "openFilesOnly",
-                autoImportCompletions = true,
+    opts = function(_, opts)
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable signature help
+      keys[#keys + 1] = { "<C-k>", false }
+
+      return vim.tbl_deep_extend("force", opts, {
+        codelens = { enabled = true },
+        server = {
+          basedpyright = {
+            settings = {
+              basedpyright = {
+                openFilesOnly = true,
+                analysis = {
+                  logLevel = "Trace",
+                  stubPath = vim.fn.stdpath("data") .. "/lazy/python-type-stubs",
+                  extraPaths = {},
+                  typeshedPaths = { vim.fn.stdpath("data") .. "/lazy/typeshed" },
+                  autoSearchPaths = true,
+                  diagnosticMode = "openFilesOnly",
+                  autoImportCompletions = true,
+                },
               },
             },
           },
         },
-      },
-    },
+      })
+    end,
   },
 
   -- cmdline tools and lsp servers
   {
     "williamboman/mason.nvim",
-    opts = {
-      ui = { border = "rounded" },
-    },
+    opts = function()
+      return {
+        ui = { border = "rounded" },
+        ensure_installed = {
+          -- LSP servers
+          "lua-language-server", -- lua
+          "json-lsp", -- json
+          "basedpyright", -- python
+          "ruff", -- python
+          "html-lsp", -- html
+          "htmx-lsp", -- htmx
+          "css-lsp", -- css
+          "bash-language-server", -- shellscript
+          "clangd", -- c, cpp, etc.
+          "copilot-language-server", -- github copilot
+          "llm-ls", -- llm
+          -- linters
+          "eslint_d", -- js, ts, jsx, tsx
+          "shellcheck", -- shellscript
+          "cpplint", -- c, cpp
+          "cspell", -- grammar, text, etc.
+          -- formatters
+          "stylua", -- lua
+          "shfmt", -- shellscript
+          "yapf", -- python
+          "isort", -- python
+          "prettierd", -- js, ts, jsx, tsx
+          "beautysh", -- shellscript
+          "clang-format", -- c, cpp, c#, etc.
+          "xmlformatter", -- xml
+          "yamlfmt", -- yaml
+          -- debuggers
+          "debugpy", -- python
+        },
+      }
+    end,
   },
 
   -- vscode-like winbar
