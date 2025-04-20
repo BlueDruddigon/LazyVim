d033e3accb59bf40128afdd5e503e00e0438bf02
BlueDruddigon <haibuihoang1999@gmail.com>
2025-04-20 18:31:10 +0700

    fix(lsp): add initial compile flag for clangd

--- lua/plugins/extras/lang/clangd.lua
+++ lua/plugins/extras/lang/clangd.lua
@@ -3,6 +3,7 @@ return {
   opts = {
     setup = {
       clangd = function(_, opts)
+        opts.init_options.fallbackFlags = { "--std=c++20" }
         opts.capabilities.offsetEncoding = { "utf-16" }
       end,
     },
